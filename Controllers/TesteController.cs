824964fa9b3a627f74f3cbeaed931d91d981498e
DanielHalfeld <dchalfeld@gmail.com>
2023-10-13 22:21:44 -0300

    adc toda a aplicação + TesteController com index

---
+++ Controllers/TesteController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleFinanceiro.Controllers
+{
+    public class TesteController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Página teste";
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ControleFinanceiro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
